868b88ac9c7b095f2522dc9c0dcd5cfeeb0cc012
Dastko <dastko@gmail.com>
2016-02-28 22:10:47 +0100

    dd

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,9 +71,8 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: 'http://javaopenness.azurewebsites.net/',
         livereload: 35729
       },
       proxies: [
